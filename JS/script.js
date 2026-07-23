1053c5a0064634f0d8b16cad3650074d2c0481fc
mohamed samy <mohamedeng747@gmil.com>
2026-07-23 19:12:34 +0300

    mohamed samy

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,9 @@
+// Import Flowbite
 import 'flowbite';
 import { initFlowbite } from 'flowbite';
 initFlowbite();
 
+// Import Swiper
 import Swiper from 'swiper';
 import 'swiper/css';
 
-
-// استدعاء موديولات التنقل والترقيم لـ Swiper
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// 2. تفعيل وتشغيل Swiper
-const swiper = new Swiper('.mySwiper', {
-  modules: [Navigation, Pagination],
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
