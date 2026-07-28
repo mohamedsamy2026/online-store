5cf7057e1c7ed62682686659ac9d77c7d919d152
mohamed samy <mohamedeng747@gmil.com>
2026-07-28 20:13:17 +0300

    mohamed

--- JS/swiper.js
+++ JS/swiper.js
@@ -1,11 +1,16 @@
-// Import Swiper
+// Import Swiper and modules 
 import Swiper from 'swiper';
-import { Autoplay, Pagination } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 
-// Hero Section Swiper start
+
+
+// 1. Hero Section Start
 const swiper = new Swiper('.swiper-home', {
     modules: [Autoplay, Pagination],
     autoplay: {
@@ -18,4 +23,29 @@ const swiper = new Swiper('.swiper-home', {
         clickable: true,
     },
 });
-// Hero Section Swiper End
+
+// 1. Hero Section End
+
+// 2. Products 1 Start
+const productsSwiper = new Swiper('.products-swiper', {
+    modules: [Autoplay, Pagination, Navigation],
+    slidesPerView: 5,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: '.next-btn',
+        prevEl: '.prev-btn',
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        320: { slidesPerView: 1 },
+        640: { slidesPerView: 2 },
+        768: { slidesPerView: 3 },
+        1024: { slidesPerView: 5 },
+    }
+});
+// 2. Products 1 End
