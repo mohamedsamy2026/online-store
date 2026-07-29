97a5d753e36de99bc4f424f00722c44541fcdc56
mohamed samy <mohamedeng747@gmil.com>
2026-07-29 21:15:27 +0300

    mohamed

--- JS/swiper.js
+++ JS/swiper.js
@@ -47,10 +47,10 @@ document.querySelectorAll('.products-swiper').forEach(swiperEl => {
         },
         loop: true,
         breakpoints: {
-            1200: { slidesPerView: 5, spaceBetween: 20 },
-            1000: { slidesPerView: 4, spaceBetween: 20 },
-            700: { slidesPerView: 3, spaceBetween: 15 },
-            0: { slidesPerView: 2, spaceBetween: 10 }
+            1200: { slidesPerView: 4, spaceBetween: 15 },
+            1000: { slidesPerView: 3, spaceBetween: 15 },
+            500: { slidesPerView: 2, spaceBetween: 10 },
+            0: { slidesPerView: 1, spaceBetween: 7 }
         }
     });
 });
